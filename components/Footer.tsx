3e8ae2c55bc1827b023d93a8e734031eb2ae29a3
Emir Risyad <emirrsyd@gmail.com>
2023-12-07 00:20:28 +0100

    added landing page

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <div className='fixed inset-x-0 bottom-0'>
+      <footer className='bg-white border-t'>
+        <div className='mx-auto py-5'>
+          <p className='text-center text-xs text-black'>This is a footer</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
