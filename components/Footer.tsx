905c14214011a25649b2916d7c982b96d7bb8c31
Emir Risyad <emirrsyd@gmail.com>
2023-12-07 19:05:41 +0100

    edit footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,10 @@ export const Footer = () => {
     <div className='fixed inset-x-0 bottom-0'>
       <footer className='bg-white border-t'>
         <div className='mx-auto py-5'>
-          <p className='text-center text-xs text-black'>This is a footer</p>
+          <p className='text-center text-xs text-black'>
+            {' '}
+            &copy; 2023 GO Shinjang. All rights reserved.
+          </p>
         </div>
       </footer>
     </div>
