d5c2b17d921903eb2c7f01f460ec7164931c811f
Emir Risyad <emirrsyd@gmail.com>
2024-01-31 19:05:36 +0100

    fixed orders table style

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -2,8 +2,6 @@ import { getCookie } from 'cookies-next';
 import payload from 'payload';
 import { cookies } from 'next/headers';
 import { ProductClient } from './client';
-import { Product } from '@/payload-types';
-import { ProductColumn } from '@/src/app/orders/columns';
 
 export default async function OrdersPage() {
   const customerName = getCookie('name', { cookies });
@@ -23,8 +21,8 @@ export default async function OrdersPage() {
   const products = result.docs[0].ordered;
 
   return (
-    <div>
-      <div className='flex flex-col items-start gap-5 self-stretch py-4 px-2'>
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
         {/* //! data should be string instead of typeProduct
          @ts-expect-error*/}
         <ProductClient data={products} />
